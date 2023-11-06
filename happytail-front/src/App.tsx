cab35b8060cbc1f096eb04ca25cd044a6e744487
AntDavi <ant.davi07@gmail.com>
2023-11-06 20:03:49 -0300

    first commit

---
+++ happytail-front/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-3xl font-bold underline'>
+        <h1>HappyTail</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
